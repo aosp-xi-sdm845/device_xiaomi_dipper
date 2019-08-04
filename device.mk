4a60b36c54558c3fab28fd5da36ba3693a6f3164
argraur <argraur@gmail.com>
2019-08-04 17:51:42 +0000

    dipper: switch to AOSP NFC stack
    Signed-off-by: argraur <argraur@gmail.com>
    Change-Id: I17e2d722b82c6262843cef72e96a9dc677fa2860

--- device.mk
+++ device.mk
@@ -69,18 +69,11 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
-    com.android.nfc_extras \
-    com.nxp.nfc.nq \
-    com.nxp.nfc.nq.xml \
-    libnqnfc-nci \
-    libnqp61-jcop-kit \
-    nfc_nci.nqx.default \
-    nqnfcee_access.xml \
-    nqnfcse_access.xml \
-    NQNfcNci \
+    android.hardware.nfc@1.1-service \
+    NfcNci \
     Tag \
-    vendor.nxp.hardware.nfc@1.1-service
+    SecureElement \
+    android.hardware.secure_element@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
