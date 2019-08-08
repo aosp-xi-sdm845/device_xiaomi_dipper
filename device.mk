dddfe4bbe389ea319ce5376f0f8bbc550dd09e79
argraur <argraur@gmail.com>
2019-08-08 10:50:16 +0000

    dipper: don't build packages unsupported by AOSP
    Signed-off-by: argraur <argraur@gmail.com>

--- device.mk
+++ device.mk
@@ -87,10 +87,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
-# PocketMode
-PRODUCT_PACKAGES += \
-    XiaomiPocketMode
-
 # Secure element
 PRODUCT_PACKAGES += \
     SecureElement
